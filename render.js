7dc314564d17d834d0e2cc8355e896f0941c6aa9
Sammy M. Waweru <witsdeveloper@outlook.com>
2023-02-15 12:57:48 +0300

    Added HTTPS on ipinfo.io URL

--- render.js
+++ render.js
@@ -11,7 +11,7 @@
 		telInput.intlTelInput({
 			initialCountry: "auto",
 			 geoIpLookup: function(callback) {
-				 $.get('http://ipinfo.io', function() {}, "jsonp").always(function(resp) {
+				 $.get('https://ipinfo.io', function() {}, "jsonp").always(function(resp) {
 				 var countryCode = (resp && resp.country) ? resp.country : "";
 				 callback(countryCode);
 				 });
